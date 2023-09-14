98e660edef974176dca54618c016f3d1490a2642
Mercy <mercywmuriithi.mm@gmail.com>
2023-09-15 00:28:31 +0300

    Lab completed

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,11 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
-    pass
+    square_num=[num for num in num_list if num%2==0] 
+    return square_num      
+    
 
 def make_exclamation(sentence_list):
+    make_exclamation=[f'{sentence}!' for sentence in sentence_list]
+    return make_exclamation
     pass
